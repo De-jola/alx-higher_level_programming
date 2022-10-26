6f39a2cecdd186e147193d37f33ba61f8bf6a79b
Esther Anuoluwapo Adejola <adejolanu@gmail.com>
2022-10-26 10:21:18 -0700

    Manipulating lists with exceptions

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    j = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            j += 1
+        except IndexError:
+            continue
+        except ValueError:
+            continue
+        except TypeError:
+            continue
+    print("")
+    return j
