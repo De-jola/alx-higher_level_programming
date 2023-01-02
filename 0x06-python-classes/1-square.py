e7f7e089472625c40d334ac3855b867bf9e67543
Esther Anuoluwapo Adejola <adejolanu@gmail.com>
2023-01-02 07:23:21 -0800

    Initialized the square class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Representing a square by a class"""
+
+
+class Square:
+    """A class that can contain any defined object to imitate a square"""
+    def __init__(self, size):
+        """Initializes size"""
+        self.__size = size
